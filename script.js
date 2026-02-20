679c2f0e6e131c95b314e718635e1fbb1e4845be
bellyfisher <527icubley@frhsd.com>
2026-02-20 10:36:00 -0500

    day three - CSS, JS - You can press numbers now !!

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+// what the user is currently typing, as text.
+let typedNumberText = ''
+
+// the number we store for calculations
+let storedNumber = null
+
+// the operator curretly selected, AKA + - * /
+let currentOperator = ''
+
+// used ONLY for displaying the history line
+// in example - ['3', '+', '4']
+let historyParts = []
+
+//-----------------
+//HELPER FUNCTIONS
+//-----------------
+
+function setStatus(message) {
+    document.getElementById('statusLine').textContent = message
+}
+
+function showSymbol(operator) {
+    if (operator === '*') return '×';
+    if (operator === '/') return '÷';
+    if (operator === '-') return '&#x2212';
+    return operator;
+}
+
+
+function updateScreen(){
+const display = document.getElementById('displayLine')
+const history = document.getElementById('historyLine')
+const status = document.getElementById('statusLine')
+
+display.textContent = typedNumberText
+
+}
+
 function pressNumber(digit) {
-    console.log(digit)
+setStatus('');
+    if (typedNumberText === '0') {
+        typedNumberText = digit;
+    } else {
+    typedNumberText = typedNumberText + digit
+    }
+    updateScreen()
 }
